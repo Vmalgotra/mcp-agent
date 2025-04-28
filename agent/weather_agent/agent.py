9352405154502fb5f1bc48fd9f69a9ee732ccab8
Vmalgotra <32836184+Vmalgotra@users.noreply.github.com>
2025-04-28 10:12:14 +0530

    Update agent.py

--- agent/weather_agent/agent.py
+++ agent/weather_agent/agent.py
@@ -1,33 +1,11 @@
 from google.adk.agents import LlmAgent
 from google.adk.tools.mcp_tool import MCPToolset
-
-
-# async def get_tools_async():
-#   """Gets tools from the File System MCP Server."""
-#   print("Attempting to connect to MCP Filesystem server...")
-#   tools, exit_stack = await MCPToolset.from_server(
-#       # Use StdioServerParameters for local process communication
-#       connection_params=StdioServerParameters(
-#           command='npx', # Command to run the server
-#           args=["-y",    # Arguments for the command
-#                 "@modelcontextprotocol/server-filesystem",
-#                 # TODO: IMPORTANT! Change the path below to an ABSOLUTE path on your system.
-#                 "/path/to/your/folder/"],
-#       )
-#       # For remote servers, you would use SseServerParams instead:
-#       # connection_params=SseServerParams(url="http://remote-server:port/path", headers={...})
-#   )
-#   print("MCP Toolset created successfully.")
-#   # MCP requires maintaining a connection to the local MCP Server.
-#   # exit_stack manages the cleanup of this connection.
-#   return tools, exit_stack
 from google.adk.tools.mcp_tool.mcp_session_manager import SseServerParams
 
 
 async def get_tools_async():
     tools, exit_stack = await MCPToolset.from_server(
         connection_params=SseServerParams(
-            # url="https://mcp-hello-world-393833795300.us-central1.run.app/sse"
             url="http://localhost:8000/sse"
         )
     )
